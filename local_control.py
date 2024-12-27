c721c6b464f201b733a33b3dff2d1df494db1a44
yuw395 <yutongwu0525@gmail.com>
2024-12-27 17:44:17 +0800

    Delete unnecessary codes

--- local_control.py
+++ local_control.py
@@ -14,23 +14,10 @@ from realworld.wrapper import ActionWrapper, DofWrapper, FrameResizeWrapper, Fra
 from collections import deque
 import cv2
 
-CAMERA_NAMES = ['cam_high', 'cam_right_wrist', 'cam_left_wrist']
-global observation_window
-observation_window = None
-# observation_window = deque(maxlen=2)
+# CAMERA_NAMES = ['cam_high', 'cam_right_wrist', 'cam_left_wrist']
+# global observation_window
+# observation_window = None
 
-# observation_window.append({
-#         'qpos': [],
-#         'images': {
-#             CAMERA_NAMES[0]: torch.zeros((480,640,3)).tolist(),
-#             CAMERA_NAMES[1]: torch.zeros((480,640,3)).tolist(),
-#             CAMERA_NAMES[2]: torch.zeros((480,640,3)).tolist(),
-#         }
-# })
-# def jpeg_mapping(img):
-#     img = cv2.imencode('.jpg', img)[1].tobytes()
-#     img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
-#     return img
 def create_ssh_client(server, port,user, password, kp='qzpubrsa'):
     client=paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -38,39 +25,45 @@ def create_ssh_client(server, port,user, password, kp='qzpubrsa'):
     client.connect(server, port=port, username=user, pkey=pk)
     return client
 
-def update_observation_window(env):
-    global observation_window
+# def update_observation_window(env):
+#     global observation_window
 
-    img_left = env.get_obs()['rdt']
-    print(f"image type:{type(img_left)}")
-    print(f"image type:{img_left.shape}")
-    if observation_window is None:
-        observation_window = deque(maxlen=2)
-        # observation_window.append({'qpos': None, 'images': {name: None for name in CAMERA_NAMES}})
-        observation_window.append({
-        'qpos': [],
-        'images': {
-            CAMERA_NAMES[0]: torch.zeros((480,640,3)).tolist(),
-            CAMERA_NAMES[1]: torch.zeros((480,640,3)).tolist(),
-            CAMERA_NAMES[2]: torch.zeros((480,640,3)).tolist(),
-            }
-        })
+#     img_left = env.get_obs()['rdt']
+#     print(f"image type:{type(img_left)}")
+#     print(f"image type:{img_left.shape}")
+#     if observation_window is None:
+#         observation_window = deque(maxlen=2)
+#         # observation_window.append({'qpos': None, 'images': {name: None for name in CAMERA_NAMES}})
+#         observation_window.append({
+#         'qpos': [],
+#         'images': {
+#             CAMERA_NAMES[0]: torch.zeros((480,640,3)).tolist(),
+#             CAMERA_NAMES[1]: torch.zeros((480,640,3)).tolist(),
+#             CAMERA_NAMES[2]: torch.zeros((480,640,3)).tolist(),
+#             }
+#         })
 
-    # img_right = jpeg_mapping(img_right)
-    img_right = np.zeros_like(img_left)
-    img_front = np.zeros_like(img_left)
-    qpos_right = env.cur_qpos
-    qpos = np.concatenate((np.zeros_like(qpos_right), qpos_right), axis=0)
-    qpos = torch.from_numpy(qpos).float()
+#     # img_right = jpeg_mapping(img_right)
+#     img_right = np.zeros_like(img_left)
+#     img_front = np.zeros_like(img_left)
+#     qpos_right = env.cur_qpos
+#     qpos = np.concatenate((np.zeros_like(qpos_right), qpos_right), axis=0)
+#     qpos = torch.from_numpy(qpos).float()
 
-    observation_window.append({
-        'qpos': qpos.tolist(),
-        'images': {
-            CAMERA_NAMES[0]: img_front.tolist(),
-            CAMERA_NAMES[1]: img_right.tolist(),
-            CAMERA_NAMES[2]: img_left.tolist(),
-        }
-    })
+#     observation_window.append({
+#         'qpos': qpos.tolist(),
+#         'images': {
+#             CAMERA_NAMES[0]: img_front.tolist(),
+#             CAMERA_NAMES[1]: img_right.tolist(),
+#             CAMERA_NAMES[2]: img_left.tolist(),
+#         }
+#     })
+def create_ssh_client(server, port,user, password, kp='qzpubrsa'):
+    client=paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    pk=paramiko.RSAKey.from_private_key_file(kp, password=password)
+    client.connect(server, port=port, username=user, pkey=pk)
+    return client
 
 def send_observations_to_server(ssh, env, remote_path):
     print('Now send observation picture to remote sensor')
@@ -83,13 +76,13 @@ def send_observations_to_server(ssh, env, remote_path):
         print('Observation picture sent to remote sensor successfully')
 
 
-def write_imaghes_to_json(observation_window,filename):
-    images_data = []
-    for obs in observation_window:
-        images = obs['images']
-        images_data.append(images)
-    with open(filename, 'w') as f:
-        json.dump(images_data, f)
+# def write_imaghes_to_json(observation_window,filename):
+#     images_data = []
+#     for obs in observation_window:
+#         images = obs['images']
+#         images_data.append(images)
+#     with open(filename, 'w') as f:
+#         json.dump(images_data, f)
 
 def send_data_to_server(ssh, data, chunk_size=100000):
     print("Sending observation data to server...")
@@ -127,27 +120,6 @@ def load_actions_from_json(filename='action.json'):
 
 def get_action_from_server(ssh, local_path='action.json'):
     print("Retrieving action data from server...")
-    # stdin, stdout, stderr = ssh.exec_command("cat ~/RoboticsDiffusionTransformer/action.json")
-    # error = stderr.read().decode('utf-8')
-    
-    # if error:
-    #     print(f"get action error: {error}")
-    #     return np.array([])  # Return empty array in case of error
-
-    # action_data = stdout.read().decode('utf-8')
-    # if action_data:
-    #     print("Action data retrieved successfully.")
-    #     try:
-    #         # Parse the JSON data
-    #         actions = load_actions_from_json(action_data)
-    #         # Assuming actions is a list of actions
-    #         return actions # Convert to NumPy array if needed
-    #     except json.JSONDecodeError as e:
-    #         print(f"Failed to parse JSON: {e}")
-    #         return np.array([])  # Return empty array in case of failure
-    # else:
-    #     print("Failed to retrieve action data.")
-    #     return np.array([])  # Return empty array in case of failure
     try:
         sftp = ssh.open_sftp()
         sftp.get('./RoboticsDiffusionTransformer/action.json', local_path)
@@ -221,19 +193,19 @@ def main(args):
     while True:
         send_observations_to_server(ssh, env, remote_path)
         send_cur_qpos(ssh, env)
-        update_observation_window(env)
+        # update_observation_window(env)
         step += 1
-        images_data = {}
+        # images_data = {}
         
-        for i, obs in enumerate(observation_window):
-            images = obs['images']
-            if i < 1:
-                for k,v in images.items():
-                    images_data[f"{k}_prev"] = v
-            else:
-                for k,v in images.items():
-                    images_data[k] = v
-        send_data_to_server(ssh, images_data)
+        # for i, obs in enumerate(observation_window):
+        #     images = obs['images']
+        #     if i < 1:
+        #         for k,v in images.items():
+        #             images_data[f"{k}_prev"] = v
+        #     else:
+        #         for k,v in images.items():
+        #             images_data[k] = v
+        # send_data_to_server(ssh, images_data)
 
         # Get language instruction from user
         instruction = input("Enter the language instruction: ")
--- remote_inference.py
+++ remote_inference.py
@@ -32,19 +32,7 @@ def jpeg_mapping(img):
     img = cv2.imencode('.jpg', img)[1].tobytes()
     img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
     return img
-# t5_tokenizer = AutoTokenizer.from_pretrained(
-#             "google/t5-v1_1-xxl",
-#             model_max_length=120, # From RDT/model/t5_encoder.py
-#             cache_dir="./cache/t5_tk_cache",
-#             local_files_only=False, # From RDT/model/t5_encoder.py
-#         )
-# t5_model = T5EncoderModel.from_pretrained(
-#             "google/t5-v1_1-xxl",
-#             cache_dir="./cache/t5_m_cache",
-#             local_files_only=False,
-#     ).eval()
 
-# t5_model = torch.nn.DataParallel(t5_model, device_ids=[args.device_1, args.device_2])
 os.environ['http_proxy'] = 'http://127.0.0.1:7890'
 os.environ['https_proxy'] = 'http://127.0.0.1:7890'
 
@@ -76,45 +64,17 @@ def contains_png_files(path):
         - a single .png file.
     Return False otherwise.
     """
-
-    # 1. Check if the path exists at all
     if not os.path.exists(path):
         return False
-
-    # 2. If the path is a file, just check its extension
     if os.path.isfile(path):
         return path.lower().endswith('.png')
-
-    # 3. If the path is a directory, list files and check
     if os.path.isdir(path):
         for filename in os.listdir(path):
             if filename.lower().endswith('.png'):
                 return True
         return False
 
-    # Fallback (unlikely case)
     return False
-
-
-def get_text_embeddings(args, texts, model):
-    tokenizer,encoder = model.get_text_enboder()
-    text_tokens_and_mask = tokenizer(
-        texts,
-        padding="longest",
-        truncation=True,
-        return_attention_mask=True,
-        add_special_tokens=True,
-        return_tensors="pt",
-    )
-
-    input_ids = text_tokens_and_mask["input_ids"].to(args.device_2)
-    attention_mask = text_tokens_and_mask["attention_mask"].to(args.device_2)
-    with torch.no_grad():
-        text_encoder_embs = encoder(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-        )["last_hidden_state"].detach()
-    return text_encoder_embs, attention_mask
 
 def inference_fn(policy, args, observation=observation_window):
     with torch.inference_mode():
@@ -145,16 +105,9 @@ def inference_fn(policy, args, observation=observation_window):
 
         # Permute dimensions back to [H, W, C]text_embedp
         img_right = img_right.permute(1, 2, 0)
-
-        # img_front = torch.zeros_like(img_right)
-        # img_left = torch.zeros_like(img_right)
         
         img_front = None
         img_left = None
-
-        # img_front_prev = torch.zeros_like(img_right)
-        # img_left_prev = torch.zeros_like(img_right)
-        # img_right_prev = torch.zeros_like(img_right)
         
         img_front_prev = None
         img_left_prev = None
@@ -174,28 +127,16 @@ def inference_fn(policy, args, observation=observation_window):
                 img_front,
                 img_right,
                 img_left]
-        # images = [observation[-2]['images'][name] for name in CAMERA_NAMES]+ [observation[-1]['images'][name] for name in CAMERA_NAMES]
-        # print(len(images))
-        # print(type(images[0]))
-        # images = [np.uint8(img * 255) if img.dtype != np.uint8 else img for img in images]
+        
         images = [PImage.fromarray(img.numpy()) if img is not None else None for img in images]
-        # print(len(images))
+
         print(proprio.size())
         lang_embeddings = policy.encode_instruction(text)
         actions = policy.step(proprio=proprio, images=images, text_embeds=lang_embeddings).squeeze(0).cpu().numpy()
         return actions
 
 def _process_action(actions):
-        # if self.args.use_actions_interpolation:
-        #     processed_actions = self._interpolate_action(action)
-        # else:
-        # actions = np.squeeze(actions)
-        # print(f"actions shape: {actions.shape}, {actions}")
         actions[..., -1] = np.where(actions[..., -1] > 0.5, 1, -1)
-        # eef_position = actions[..., :3]
-        # eef_euler = actions[..., 3:6]
-
-        # return (eef_position, eef_euler, gripper_action)
         return actions
 
 def main(args):
